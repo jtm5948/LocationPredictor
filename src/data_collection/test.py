ebd8f9be3eef15a2c3c455be38240f095704ab3b
Justin Maines <jtm5948@psu.edu>
2023-07-01 20:49:37 -0400

    first commit

---
+++ src/data_collection/test.py
@@ -0,0 +1,17 @@
+from streetview import search_panoramas
+from streetview import get_streetview
+from streetview import get_panorama
+panos = search_panoramas(lat = 41.011782, lon = -78.435571)
+first = panos[4].pano_id
+second = panos
+'''
+print(first)
+image = get_streetview(
+    pano_id = first,
+    api_key = "AIzaSyArqq6R88oxCc9rJx5RdYKP4BwEPyJNuoo"
+)
+
+image.save("image2.jpg", "jpeg")
+'''
+image = get_panorama(pano_id = first)
+image.save("pano.jpg", "jpeg")
